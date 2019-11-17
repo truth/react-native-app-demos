5e1f6815e6d48f2782dfb9d0dc68560479d5a4a0
wzl <wzl@gkmobile.tv>
2019-11-17 11:22:32 +0800

    AwesomeProject

---
+++ AwesomeProject/android/app/src/main/java/com/host/DispatchActivity.java
@@ -0,0 +1,27 @@
+package com.host;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.host.utils.DispatchUtils;
+
+public class DispatchActivity extends ReactActivity {
+
+    public static void start(Context context) {
+        Intent starter = new Intent(context, DispatchActivity.class);
+        starter.setAction(android.content.Intent.ACTION_DEFAULT);
+        starter.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+        starter.addFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        context.startActivity(starter);
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+
+        DispatchDelegate delegate = new DispatchDelegate(this, DispatchUtils.dispatchModel);
+
+        return delegate;
+    }
+}
